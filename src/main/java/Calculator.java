848ec7a3a66723cc707c869a3825726ca1121099
Jonny Tullis <jon.tullis558@gmail.com>
2020-10-09 18:21:50 -0600

    Added test console output

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -67,6 +67,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
+        System.out.println("Test console output");
         return n + UUID.randomUUID();
     }
 
